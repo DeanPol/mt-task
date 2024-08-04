d36434d9a29f67f2fd4f095b05dcd3f5f4e17dcb
dionysius <dionysius.politis@raketech.com>
2024-08-04 10:37:30 +0900

    class definition

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # mt-task
 
 ## 0.1 initial commit setting up tsc and npm
+
+## 0.2 classes, add item, return cart
--- index.ts
+++ index.ts
@@ -1,2 +1,30 @@
-let test: string = "Hello World";
-console.log(test);
+interface ProductInterface {
+  id: string;
+  price: number;
+}
+
+export class Cart {
+  private itemList: ProductInterface[];
+  private total: number;
+
+  constructor() {
+    this.itemList = [];
+    this.total = 0;
+  }
+
+  addItem(item: ProductInterface) {
+    this.itemList.push(item);
+    this.total += item.price;
+  }
+
+  getCart(): [ProductInterface[], number] {
+    return [this.itemList, this.total];
+  }
+}
+
+export class Product implements ProductInterface {
+  constructor(public id: string, public price: number) {
+    this.id = id;
+    this.price = price;
+  }
+}
