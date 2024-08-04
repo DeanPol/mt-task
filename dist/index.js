1686bbdbb2ad93d4444020943eef287c9b09e460
dionysius <dionysius.politis@raketech.com>
2024-08-04 10:38:31 +0900

    Update dist

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,26 @@
 "use strict";
-let test = "Hello World";
-console.log(test);
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Product = exports.Cart = void 0;
+class Cart {
+    constructor() {
+        this.itemList = [];
+        this.total = 0;
+    }
+    addItem(item) {
+        this.itemList.push(item);
+        this.total += item.price;
+    }
+    getCart() {
+        return [this.itemList, this.total];
+    }
+}
+exports.Cart = Cart;
+class Product {
+    constructor(id, price) {
+        this.id = id;
+        this.price = price;
+        this.id = id;
+        this.price = price;
+    }
+}
+exports.Product = Product;
